bfc4cdf4c521e59420036b0a0c7defd9faf5af84
InterurbanDev <72585301+InterurbanDev@users.noreply.github.com>
2026-03-11 17:00:46 -0400

    Update automatic publishing
    Add Modrinth to automatic publishing
    Add CurseForge to automatic publishing
    Update mod dependencies
    Add CHANGELOG.md
    Fix data generation on 1.18 causing bad Forgix merge
    Remove maven publishing
    
    Signed-off-by: InterurbanDev <72585301+InterurbanDev@users.noreply.github.com>

--- 1.18/forge/src/main/java/dev/interurban/forge/datagen/ModBlockStateProvider.java
+++ 1.18/forge/src/main/java/dev/interurban/forge/datagen/ModBlockStateProvider.java
@@ -1,27 +1,85 @@
 package dev.interurban.forge.datagen;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import dev.architectury.registry.registries.RegistrySupplier;
 import dev.interurban.RailroadBlocks;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
+import net.minecraft.data.HashCache;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ConfiguredModel;
+import net.minecraftforge.client.model.generators.IGeneratedBlockstate;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
 import java.util.function.Function;
 
 import static dev.interurban.datagen.ModelHandler.horizontalBlockList;
 import static dev.interurban.datagen.ModelHandler.nonCubeBlockList;
 
+/**
+ * Generates blockstates for the mod's custom blocks
+ */
 public class ModBlockStateProvider extends BlockStateProvider {
+
+    // Contains variables for overriding the run() function of BlockStateProvider
+    private static final Gson GSON = new GsonBuilder()
+            .setPrettyPrinting()
+            .disableHtmlEscaping() // Prevents an issue with special characters in the JSON files
+            .create();
+    private final DataGenerator generator;
+
+    /**
+     * Starts the blockstate provider with overrides and saves the data generator
+     *
+     * @param generator          the data generator
+     * @param existingFileHelper checks if other data files exist
+     */
     public ModBlockStateProvider(DataGenerator generator, ExistingFileHelper existingFileHelper) {
         super(generator, RailroadBlocks.MOD_ID, existingFileHelper);
+
+        this.generator = generator;
     }
 
+    /**
+     * Overrides the run() function of BlockStateProvider to allow us to change the order of block variants for Forgix
+     * merge
+     *
+     * @param cache HashCache
+     * @throws IOException IOException
+     */
+    @Override
+    public void run(HashCache cache) throws IOException {
+        super.run(cache);
+
+        Path outputFolder = generator.getOutputFolder(); // Gets the original output from datagen
+
+        // For each registered block state
+        for (Map.Entry<Block, IGeneratedBlockstate> entry : registeredBlocks.entrySet()) {
+            // Gets path to target blockstate files
+            ResourceLocation blockId = ForgeRegistries.BLOCKS.getKey(entry.getKey());
+            assert blockId != null;
+            Path target = outputFolder.resolve("assets/" + blockId.getNamespace() + "/blockstates/" + blockId.getPath() + ".json");
+
+            JsonObject original = entry.getValue().toJson(); // Gets original generated JSON file
+            JsonObject sorted = sortBlockStateJson(original); // Sorts JSON file data
+            DataProvider.save(GSON, cache, sorted, target); // Saves modified JSON file
+        }
+    }
+
+    /**
+     * Registers all blocks
+     */
     @Override
     protected void registerStatesAndModels() {
         for (RegistrySupplier<? extends Block> block : horizontalBlockList) {
@@ -32,16 +90,33 @@ public class ModBlockStateProvider extends BlockStateProvider {
         }
     }
 
+    /**
+     * Generates a horizontal blockstate for a block with an existing model
+     *
+     * @param block the block to generate a horizontal blockstate for
+     * @param model the block's model
+     */
+    private void betterHorizontalBlock(Block block, ModelFile model) {
+        betterHorizontalBlock(block, $ -> model);
+    }
+
+    /**
+     * Finds and returns the existing model for a block
+     *
+     * @param block the block to find the model for
+     * @return the model for the given block
+     */
     private ModelFile getModel(RegistrySupplier<? extends Block> block) {
         ResourceLocation id = block.getId();
 
         return models().getExistingFile(modLoc("block/" + id.getPath()));
     }
 
-    private void betterHorizontalBlock(Block block, ModelFile model) {
-        betterHorizontalBlock(block, $ -> model);
-    }
-
+    /**
+     * Generates a horizontal blockstate for a block with an existing model
+     * @param block the block to generate a horizontal blockstate for
+     * @param modelFunc the block's existing model and blockstate
+     */
     private void betterHorizontalBlock(Block block, Function<BlockState, ModelFile> modelFunc) {
         getVariantBuilder(block)
                 .forAllStatesExcept(state -> ConfiguredModel.builder()
@@ -50,5 +125,27 @@ public class ModBlockStateProvider extends BlockStateProvider {
                                 .build(),
                         BlockStateProperties.WATERLOGGED
                 );
+    }
+
+    /**
+     * Sorts the data values in a JSON file to match other mod loaders
+     *
+     * @param original the original generated JSON data
+     * @return the sorted version of the generated JSON data
+     */
+    private JsonObject sortBlockStateJson(JsonObject original) {
+        JsonObject result = new JsonObject();
+
+        // Sorts variants alphabetically to match other mod loaders formatting for data generation
+        if (original.has("variants")) {
+            JsonObject variants = original.getAsJsonObject("variants");
+            JsonObject sortedVariants = new JsonObject();
+            variants.entrySet().stream()
+                    .sorted(Map.Entry.comparingByKey())
+                    .forEach(e -> sortedVariants.add(e.getKey(), e.getValue()));
+            result.add("variants", sortedVariants);
+        }
+
+        return result;
     }
 }
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,28 @@
-## Test Version
+## 📄 Alpha Change Log
 
-## Not Intended for Public Release
+Due to significant reworks behind the scenes and plans to release additional features in the near future, this release
+is marked as an alpha release. Bug fixes and new features may be released rapidly and with little or no testing. **It is
+recommended to backup your saves** and let us know if you experience any issues with this release!
 
-This is a test of automatic publishing to streamline future development.
+***
 
-This is **NOT** a new update intended for you to download. Please look for a different version to install.
+**This release tests our automatic publishing system!** You may see this release appear and disappear as testing is
+performed or mod details may be slightly incorrect.
+
+Note: There is no need to update to this version at this time, so you can safely remain on older versions while testing
+is performed.
+
+***
+
+### ⚙️ Improvements
+
+- Updated mod dependencies on some versions.
+
+### 🔧 Developer Changes
+
+- Fixed Forge datagen on 1.16 to allow running all 1.16+ versions on modern Java.
+- Improved some of the GitHub files, such as the code of conduct and contributing guidelines.
+- Added automatic build testing to GitHub Actions, to ensure the mod at least compiles successfully.
+- Added automatic publishing to GitHub Actions to improve update speed.
+- Fixed data generation on 1.18 causing a bad Forgix merge.
+- Removed Maven publishing references, as Maven publishing is currently not being performed.
